a3711bcfa4c8eec1faa0724afeb5f1b2a1c17d46
AkbekovNursultan <nursultan.akbekov@alatoo.edu.kg>
2023-11-18 01:07:07 +0600

    added Input validation + code clean up

--- src/Main.java
+++ src/Main.java
@@ -4,22 +4,35 @@ public class Main {
     public static void main(String[] args) {
        Scanner sc = new Scanner(System.in);
        Random ran = new Random();
-       int firstBox = ran.nextInt(200)+100;
-       int secondBox = ran.nextInt(200)+100;
-       int thirdBox = ran.nextInt(200)+100;
-       while(firstBox + secondBox + thirdBox != 713){
-           firstBox = ran.nextInt(200)+100;
-           secondBox = ran.nextInt(200)+100;
-           thirdBox = ran.nextInt(200)+100;
-       }
+       int firstBox = ran.nextInt(200) + 100;
+       int secondBox = ran.nextInt(200) + 100;
+       int thirdBox = 713 - firstBox- secondBox;
        int[] possibleLocations = new int[6];
        locations(possibleLocations);
-        putBox(firstBox, possibleLocations);
-        putBox(secondBox, possibleLocations);
-        putBox(thirdBox, possibleLocations);
-        System.out.println(firstBox + " " + secondBox + " " + thirdBox);
-        for(int i = 0; i < 6; i++){
-            System.out.print(possibleLocations[i] + " ");
+       putBox(firstBox, possibleLocations);
+       putBox(secondBox, possibleLocations);
+       putBox(thirdBox, possibleLocations);
+       System.out.println(firstBox + " " + secondBox + " " + thirdBox);
+       for(int i = 0; i < 6; i++){
+           System.out.print(possibleLocations[i] + " ");
+       }
+       System.out.println("Enter the coordinates from 1 to 6: ");
+       int firstGuess = sc.nextInt()-1;
+       while(firstGuess < 0 || firstGuess > 6){
+           System.out.println("Invalid coordinates. ");
+           firstGuess = sc.nextInt()-1;
+       }
+       System.out.println("Enter the second coordinate from 1 to 6: ");
+       int secondGuess = sc.nextInt()-1;
+       while(secondGuess < 0 || secondGuess > 6 || secondGuess == firstGuess){
+           System.out.println("Invalid coordinates. ");
+           secondGuess = sc.nextInt()-1;
+       }
+       System.out.println("Enter the third coordinate from 1 to 6: ");
+       int thirdGuess = sc.nextInt()-1;
+        while(thirdGuess < 0 || thirdGuess > 6 || thirdGuess == firstGuess || thirdGuess == secondGuess){
+            System.out.println("Invalid coordinates. ");
+            thirdGuess = sc.nextInt()-1;
         }
     }
     static void putBox(int box, int[] possibleLocations){
