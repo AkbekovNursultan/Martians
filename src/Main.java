80b14ff1c289d3634a824209c01f81701f14ec7b
AkbekovNursultan <nursultan.akbekov@alatoo.edu.kg>
2023-11-18 00:17:27 +0600

    added Cargo division

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.util.Random;
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       Scanner sc = new Scanner(System.in);
+       Random ran = new Random();
+       int firstBox = ran.nextInt(200)+100;
+       int secondBox = ran.nextInt(200)+100;
+       int thirdBox = ran.nextInt(200)+100;
+       while(firstBox + secondBox + thirdBox != 713){
+           firstBox = ran.nextInt(200)+100;
+           secondBox = ran.nextInt(200)+100;
+           thirdBox = ran.nextInt(200)+100;
+       }
+        System.out.println(firstBox + " " + secondBox + " " + thirdBox);
     }
 }
