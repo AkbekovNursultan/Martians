f5144fb0a180830ce8b75edcd80a6fc64541212a
AkbekovNursultan <nursultan.akbekov@alatoo.edu.kg>
2023-11-18 00:42:29 +0600

    added Cargo Placement

--- src/Main.java
+++ src/Main.java
@@ -12,6 +12,27 @@ public class Main {
            secondBox = ran.nextInt(200)+100;
            thirdBox = ran.nextInt(200)+100;
        }
+       int[] possibleLocations = new int[6];
+       locations(possibleLocations);
+        putBox(firstBox, possibleLocations);
+        putBox(secondBox, possibleLocations);
+        putBox(thirdBox, possibleLocations);
         System.out.println(firstBox + " " + secondBox + " " + thirdBox);
+        for(int i = 0; i < 6; i++){
+            System.out.print(possibleLocations[i] + " ");
+        }
+    }
+    static void putBox(int box, int[] possibleLocations){
+        Random ran = new Random();
+        int place = ran.nextInt(5);
+        while(possibleLocations[place] != 0) {
+            place = ran.nextInt(5);
+        }
+        possibleLocations[place] = box;
+    }
+    static void locations(int [] possibleLocations){
+        for(int i = 0; i < 6; i++){
+            possibleLocations[i] = 0;
+        }
     }
 }
