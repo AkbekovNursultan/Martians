0db9cc2b024e458e88484e0824c80b95baf36ae4
AkbekovNursultan <nursultan.akbekov@alatoo.edu.kg>
2023-11-20 21:40:32 +0600

    add foundBoxesNumber method(Program keeps running until right answers won't be entered)

--- src/Main.java
+++ src/Main.java
@@ -7,33 +7,44 @@ public class Main {
        int firstBox = ran.nextInt(200) + 100;
        int secondBox = ran.nextInt(200) + 100;
        int thirdBox = 713 - firstBox- secondBox;
-       int[] possibleLocations = new int[6];
-       locations(possibleLocations);
-       putBox(firstBox, possibleLocations);
-       putBox(secondBox, possibleLocations);
-       putBox(thirdBox, possibleLocations);
-       System.out.println(firstBox + " " + secondBox + " " + thirdBox);
-       for(int i = 0; i < 6; i++){
-           System.out.print(possibleLocations[i] + " ");
+       while(true) {
+           int[] possibleLocations = new int[6];
+           locations(possibleLocations);
+           putBox(firstBox, possibleLocations);
+           putBox(secondBox, possibleLocations);
+           putBox(thirdBox, possibleLocations);
+           System.out.println("\nEnter the first coordinate from 1 to 6: ");
+           int firstGuess = sc.nextInt() - 1;
+           while (firstGuess < 0 || firstGuess > 6) {
+               System.out.println("Invalid coordinates. ");
+               firstGuess = sc.nextInt() - 1;
+           }
+           System.out.println("Enter the second coordinate from 1 to 6: ");
+           int secondGuess = sc.nextInt() - 1;
+           while (secondGuess < 0 || secondGuess > 6 || secondGuess == firstGuess) {
+               System.out.println("Invalid coordinates. ");
+               secondGuess = sc.nextInt() - 1;
+           }
+           System.out.println("Enter the third coordinate from 1 to 6: ");
+           int thirdGuess = sc.nextInt() - 1;
+           while (thirdGuess < 0 || thirdGuess > 6 || thirdGuess == firstGuess || thirdGuess == secondGuess) {
+               System.out.println("Invalid coordinates. ");
+               thirdGuess = sc.nextInt() - 1;
+           }
+           int boxesNumber = foundBoxesNumber(possibleLocations, firstBox, secondBox, thirdBox, firstGuess, secondGuess, thirdGuess);
+           if(boxesNumber == 3){
+               break;
+           } else if(boxesNumber == 2) {
+               System.out.println("You got 2 right!");
+           } else if (boxesNumber == 1) {
+               System.out.println("You got 1 right");
+           } else {
+               System.out.println("Damn you are bad at it!");
+           }
        }
-       System.out.println("Enter the coordinates from 1 to 6: ");
-       int firstGuess = sc.nextInt()-1;
-       while(firstGuess < 0 || firstGuess > 6){
-           System.out.println("Invalid coordinates. ");
-           firstGuess = sc.nextInt()-1;
-       }
-       System.out.println("Enter the second coordinate from 1 to 6: ");
-       int secondGuess = sc.nextInt()-1;
-       while(secondGuess < 0 || secondGuess > 6 || secondGuess == firstGuess){
-           System.out.println("Invalid coordinates. ");
-           secondGuess = sc.nextInt()-1;
-       }
-       System.out.println("Enter the third coordinate from 1 to 6: ");
-       int thirdGuess = sc.nextInt()-1;
-        while(thirdGuess < 0 || thirdGuess > 6 || thirdGuess == firstGuess || thirdGuess == secondGuess){
-            System.out.println("Invalid coordinates. ");
-            thirdGuess = sc.nextInt()-1;
-        }
+        System.out.println("Yaay! U found them all!");
+        System.out.println("Their weights are:");
+        System.out.println(firstBox + " " + secondBox + " " + thirdBox);
     }
     static void putBox(int box, int[] possibleLocations){
         Random ran = new Random();
@@ -47,5 +58,16 @@ public class Main {
         for(int i = 0; i < 6; i++){
             possibleLocations[i] = 0;
         }
+    }
+    static int foundBoxesNumber(int[] possibleLocations, int firstBox, int secondBox, int thirdBox, int firstGuess, int secondGuess, int thirdGuess){
+        int sum = possibleLocations[firstGuess] + possibleLocations[secondGuess] + possibleLocations[thirdGuess];
+        if(sum == 713){
+            return 3;
+        } else if (sum == 713 - firstBox || sum == 713 - secondBox || sum == 713 - thirdBox) {
+            return 2;
+        } else if (sum == 713 - firstBox - secondBox || sum == 713 - secondBox - thirdBox || sum == 713 - firstBox - thirdBox) {
+            return 1;
+        }
+        return 0;
     }
 }
